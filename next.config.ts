9e661f1086cec96824bdf8c8de187b005aed40de
shahnawroz <sh.nawrose@gmail.com>
2025-07-30 04:55:40 +0600

    trying fixing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
- output: 'export',
+  output: "export",
+  basePath: isProd ? "/10ms_Assesment" : "",
+  assetPrefix: isProd ? "/10ms_Assesment/" : "",
   images: {
     unoptimized: true,
     domains: ["cdn.10minuteschool.com"],
