f0084e4055811849cc423dc3721db3aae1e9140c
shahnawroz <sh.nawrose@gmail.com>
2025-07-30 04:41:24 +0600

    fixed tyring

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   output: 'export',
   images: {
+    unoptimized: true,
     domains: ["cdn.10minuteschool.com"],
   },
 };
